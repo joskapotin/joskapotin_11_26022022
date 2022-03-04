bebd3794c324bfd0da27052e1d1dc8d3b820aa4a
joska <jpotin@gmail.com>
2022-03-04 19:33:43 +0100

    loading default true

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 function useFetch(url) {
   const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
 
   useEffect(() => {
