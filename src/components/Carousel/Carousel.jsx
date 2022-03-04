550d3aaff347389b66a6b698601b0dfe83f1b6a3
joska <jpotin@gmail.com>
2022-03-04 12:09:04 +0100

    livrable

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './Carousel.css'
 
 function Carousel({ theme = 'carousel--default', className = '', pictures }) {
   const [current, setCurrent] = useState(0)
+  const [currents, setCurrents] = useState([0])
   const length = pictures.length
+
+  useEffect(() => {
+    if (length > 2) {
+      if (current === 0) setCurrents([length - 1, current, current + 1])
+      else if (current === length - 1) setCurrents([current - 1, current, 0])
+      else setCurrents([current - 1, current, current + 1])
+    } else if (length === 2) setCurrents([current, current + 1])
+    else setCurrents([current])
+  }, [current, length])
 
   const prevSlide = () => {
     setCurrent(current === 0 ? length - 1 : current - 1)
@@ -14,27 +24,20 @@ function Carousel({ theme = 'carousel--default', className = '', pictures }) {
     setCurrent(current === length - 1 ? 0 : current + 1)
   }
 
-  const isLoaded = (index) => {
-    if (index === current) return true
-    return false
-  }
-
-  const pictureElements = pictures.map((picture, index) => (
-    <div className="carousel__item" key={`carousel-${index}`}>
-      {isLoaded(index) && (
-        <img className="carousel__img" src={picture} alt="" />
-      )}
+  const pictureElements = currents.map((value) => (
+    <div className="carousel__item" key={`carousel-${pictures[value]}`}>
+      <img className="carousel__img" src={pictures[value]} alt="" />
     </div>
   ))
   return (
     <div className={`carousel ${theme} ${className}`}>
+      {pictureElements}
       <button onClick={prevSlide} className="carousel__btn carousel__btn-prev">
         <span className="carousel__btn__text">previous picture</span>
       </button>
       <button onClick={nextSlide} className="carousel__btn carousel__btn-next">
         <span className="carousel__btn__text">next picture</span>
       </button>
-      {pictureElements}
     </div>
   )
 }
